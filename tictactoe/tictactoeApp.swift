d566da42fc51a16620397de6bf0c14ce7ec242bf
Saimon <saimonventura@gmail.com>
2022-12-19 21:30:03 -0300

    Initial Commit

---
+++ tictactoe/tictactoeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tictactoeApp.swift
+//  tictactoe
+//
+//  Created by GhostMon on 12/19/22.
+//
+
+import SwiftUI
+
+@main
+struct tictactoeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
